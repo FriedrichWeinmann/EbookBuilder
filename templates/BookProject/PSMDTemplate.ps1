02b0a91917b21ee8d6e29fc2ba0161c5d69ee515
Friedrich Weinmann <FriedrichWeinmann@users.noreply.github.com>
2024-10-05 15:29:56 +0200

    updates

--- templates/BookProject/PSMDTemplate.ps1
+++ templates/BookProject/PSMDTemplate.ps1
@@ -1,8 +1,8 @@
 ﻿@{
 	TemplateName = 'BookProject' # Insert name of template
-	Version = "1.0.0" # Version to build to
+	Version = "1.0.1" # Version to build to
 	AutoIncrementVersion = $true # If a newer version than specified is present, instead of the specified version, make it one greater than the existing template
-	Tags = @() # Insert Tags as desired
+	Tags = @('book') # Insert Tags as desired
 	Author = 'Friedrich Weinmann' # The author of the template, not the file / project created from it
 	Description = 'Ebook Project for authoring a book or synchronizing a book from RoyalRoad. Includs building to eBook format' # Try describing the template
 	Exclusions = @() # Contains list of files - relative path to root - to ignore when building the template
